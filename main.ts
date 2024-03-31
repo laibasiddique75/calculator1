1d1c774636f9701531955335bf2e01bb3e1080d2
laibasiddique75 <laibasiddique111@gmail.com>
2024-03-31 22:33:50 +0500

    done

---
+++ main.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+    message:"Enter first number",
+    type:"number",
+    name:"firstNumber"
+},
+{
+message:"Enter second number",
+type:"number",
+name:"secondNumber"
+},
+
+{
+    message:"select one of the operator to perform action ",
+    type:"list",
+    name:"operator",
+    choices:["addition","substraction","multiplication","division"],
+    },
+    
+]);
+
+//conditional statement
+if(answer.operator === "addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "substraction"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "multiplication"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "division"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
